d7f3c969df138a67438670a977753cecf9efc31f
Jonathan Beliën <jbe@geo6.be>
2018-03-21 12:27:28 +0100

    Fix URL

--- config/routes.php
+++ config/routes.php
@@ -7,13 +7,13 @@ use Zend\Expressive\Application;
 use Zend\Expressive\MiddlewareFactory;
 
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
-    $app->get('/app/check-points-auth/login/', App\Handler\LoginHandler::class, 'login');
-    $app->post('/app/check-points-auth/login/', App\Handler\LoginHandler::class, 'login.post');
+    $app->get('/app/check-points/login/', App\Handler\LoginHandler::class, 'login');
+    $app->post('/app/check-points/login/', App\Handler\LoginHandler::class, 'login.post');
 
-    $app->get('/app/check-points-auth/', App\Handler\HomeHandler::class, 'home');
-    $app->get('/app/check-points-auth/check/', App\Handler\CheckHandler::class, 'check');
+    $app->get('/app/check-points/', App\Handler\HomeHandler::class, 'home');
+    $app->get('/app/check-points/check/', App\Handler\CheckHandler::class, 'check');
 
-    $app->get('/app/check-points-auth/api/object/{id}', App\Handler\API\ObjectHandler::class, 'api.object');
-    $app->put('/app/check-points-auth/api/object/{id}', App\Handler\API\ObjectHandler::class, 'api.object.put');
-    $app->get('/app/check-points-auth/api/statistics/', App\Handler\API\StatisticsHandler::class, 'api.statistics.put');
+    $app->get('/app/check-points/api/object/{id}', App\Handler\API\ObjectHandler::class, 'api.object');
+    $app->put('/app/check-points/api/object/{id}', App\Handler\API\ObjectHandler::class, 'api.object.put');
+    $app->get('/app/check-points/api/statistics/', App\Handler\API\StatisticsHandler::class, 'api.statistics.put');
 };
